c01602e6f142b85b4211d42ddf2cada2270d2847
bkojusner <bkojusner@ufl.edu>
2021-11-07 23:41:30 -0500

    Ale sucks

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ async def echo(websocket, path):
     async for message in websocket:
         await websocket.send(message)
 
-start_server = websockets.serve(echo, "", int(3000))
+start_server = websockets.serve(echo, "", int(os.environ["PORT"]))
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
