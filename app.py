6ca64f88567c748e4752a1b9432a315211d3e7e4
bkojusner <bkojusner@ufl.edu>
2021-11-09 12:14:49 -0500

    Added ratelimit

--- app.py
+++ app.py
@@ -4,15 +4,22 @@ import asyncio
 import os
 import time
 import websockets
+from ratelimit import limits
 
+@limits(calls=1, period=900)
 async def echo(websocket, path):
     async for message in websocket:
-        await websocket.send(message)
-        await websocket.send("StartRec")
-        await asyncio.sleep(3)
-        await websocket.send("StopRec")
+        # If message == from computer
+        # Else do this
+        for i in range(1, 4):
+            await websocket.send(message)
+            print("starting")
+            await websocket.send("StartRec")
+            await asyncio.sleep(i)
+            print("stopping")
+            await websocket.send("StopRec")
 
-start_server = websockets.serve(echo, "", int(os.environ["PORT"]))
+start_server = websockets.serve(echo, "", 3000)#int(os.environ["PORT"]))
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
